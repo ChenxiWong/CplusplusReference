51ee2c20176c65fad8c3610db33544238f4ee484
wangchenxi <chinawangchenxi@gmail.com>
2017-05-16 17:33:05 +0800

    vector example code

---
+++ vector/test_vector.cpp
@@ -0,0 +1,28 @@
+// Last Update:2017-05-16 10:05:01
+/**
+ * @file test_vector.cpp
+ * @brief 测试vector's Muthod.
+ * @author wangchenxi
+ * @version 0.1.00
+ * @date 2017-05-16
+ */
+#include "test_vector.h"
+
+#define test_work(m)  \
+    do \
+    { \
+        m::test_main( argc, argv); \
+    } while(0)
+
+int main(int argc, char* argv[])
+{
+    std::string str("");
+
+    std::cout<<"test rbegin_rend , input: reverse"<<std::endl;
+    std::cin>>str;
+    if(str == "reverse")
+    {
+        test_work(rbegin_rend);
+    }
+    return 0;
+}
---
+++ vector/test_vector.h
@@ -0,0 +1,34 @@
+// Last Update:2017-05-16 10:05:01
+/**
+ * @file test_vector.h
+ * @brief vector Mathod come true.
+ * @author wangchenxi
+ * @version 0.1.00
+ * @date 2017-05-16
+ */
+
+#ifndef TEST_VECTOR_H
+#define TEST_VECTOR_H
+
+#include <iostream>
+#include <vector>
+
+namespace rbegin_rend
+{
+    int test_main(int argc, char* argv[])
+    {
+        std::vector<int> myvector(5); // 5 default-constructed ints;
+
+        int i = 0;
+
+        for(std::vector<int>::reverse_iterator iter = myvector.rbegin(); iter != myvector.rend(); ++iter)
+            std::cout << ' '<< *iter ;
+        std::cout << '\n';
+
+        return 0;
+    }
+}
+
+
+
+#endif  /*TEST_VECTOR_H*/
